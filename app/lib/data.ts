290d56d81b5052550f84c9f65ac35232294d850a
zohan22 <amonzabels@gmail.com>
2024-09-12 19:01:52 -0400

    Cuadros

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,11 +11,9 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
